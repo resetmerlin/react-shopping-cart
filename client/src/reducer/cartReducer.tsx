425dafe47ef3c254040ba5a3d34bd9a46ef7f38b
resetmerlin <resetmerlin@gmail.com>
2024-03-08 18:39:27 +0900

    feat: Add cart Reducer

---
+++ client/src/reducer/cartReducer.tsx
@@ -0,0 +1,97 @@
+/* eslint-disable @typescript-eslint/default-param-last */
+import {
+  CART_REQUEST,
+  CART_SUCCESS,
+  CART_FAIL,
+  CART_ADD_REQUEST,
+  CART_ADD_SUCCESS,
+  CART_ADD_FAIL,
+  CART_DELETE_REQUEST,
+  CART_DELETE_SUCCESS,
+  CART_DELETE_FAIL,
+} from '../constants';
+import { CartAction, CartAddAction, CartDeleteAction } from '../actions';
+import { Cart } from '../types';
+
+type CartsState = {
+  carts: Cart[];
+  loading: boolean;
+  error?: string | null;
+};
+
+const cartsInitialState: CartsState = {
+  carts: [],
+  loading: true,
+  error: null,
+};
+
+export const cartsReducers = (
+  state: CartsState = cartsInitialState,
+  action: CartAction
+) => {
+  switch (action.type) {
+    case CART_REQUEST:
+      return { ...state, loding: true, error: null };
+
+    case CART_SUCCESS:
+      return {
+        loading: false,
+        carts: action.payload,
+        error: null,
+      };
+
+    case CART_FAIL:
+      return { loading: false, error: action.payload, carts: [] };
+
+    default:
+      return state;
+  }
+};
+const cartAddInitialState = {};
+
+export const cartAddReducers = (
+  state = cartAddInitialState,
+  action: CartAddAction
+) => {
+  switch (action.type) {
+    case CART_ADD_REQUEST:
+      return { ...state, loding: true, error: null };
+
+    case CART_ADD_SUCCESS:
+      return {
+        loading: false,
+        carts: action.payload,
+        error: null,
+      };
+
+    case CART_ADD_FAIL:
+      return { loading: false, error: action.payload };
+
+    default:
+      return state;
+  }
+};
+const cartDeleteInitialState = {};
+
+export const cartDeleteReducers = (
+  state = cartDeleteInitialState,
+  action: CartDeleteAction
+) => {
+  switch (action.type) {
+    case CART_DELETE_REQUEST:
+      return { ...state, loding: true, error: null };
+
+    case CART_DELETE_SUCCESS:
+      return {
+        loading: false,
+        carts: action.payload,
+        error: null,
+      };
+
+    case CART_DELETE_FAIL:
+      return { loading: false, error: action.payload };
+
+    default:
+      return state;
+  }
+};
