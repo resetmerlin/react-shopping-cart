83a3162573237ed0623280e9c28474bf28ac4364
resetmerlin <resetmerlin@gmail.com>
2024-03-10 17:21:15 +0900

    feat: Add Provider on main

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Provider } from 'react-redux';
+import App from './App';
+import './_setting/index.scss';
+import store from './store';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
