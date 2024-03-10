4e2fb9f80d8b5f3407a3af85f79a59f70ff1915c
resetmerlin <resetmerlin@gmail.com>
2024-03-10 17:28:12 +0900

    feat: Add annotation and addToCart function

--- client/src/page/product/ProductPage.tsx
+++ client/src/page/product/ProductPage.tsx
@@ -3,38 +3,53 @@ import { useEffect, useMemo } from 'react';
 import { Header, Loader, Product, ProductItemDetails } from '../../components';
 import { useAppDispatch, useAppSelector } from '../../hooks';
 import { Product as IProduct } from '../../types';
-import { productAction } from '../../actions';
+import { cartAddAction, productAction } from '../../actions';
 
 export default function ProductPage() {
   const params = useParams();
   const navigate = useNavigate();
   const dispatch = useAppDispatch();
-  const id = params?.id ? +params.id : undefined;
+  const productId = params?.id ? +params.id : undefined;
   const productsInfo = useAppSelector((state) => state.productsInfo);
   const productInfo = useAppSelector((state) => state.productInfo);
-
   const { products } = productsInfo;
   const { product: singleProduct } = productInfo;
 
+  /** product value using products; multiple product data */
   const product = useMemo(
     () =>
-      [...products] && ([...products].find((val) => val.id === id) as IProduct),
-    [id, products]
+      [...products] &&
+      ([...products].find((val) => val.id === productId) as IProduct),
+    [productId, products]
   );
-  useEffect(() => {
-    if (!id) navigate('/');
-  }, [id, navigate]);
 
+  // if there is no params, navigate to home
+  useEffect(() => {
+    if (!productId) navigate('/');
+  }, [productId, navigate]);
+
+  // if there is no single product data, fetch single product
   useEffect(() => {
     let ignore = false;
-    if (!ignore && !product && id) {
-      dispatch(productAction(id));
+    if (!ignore && !product && productId) {
+      dispatch(productAction(productId));
     }
 
     return () => {
       ignore = true;
     };
-  }, [dispatch, id, product, products]);
+  }, [dispatch, productId, product, products]);
+
+  const addToCart = (
+    name: string,
+    id: number,
+    price: number,
+    imageUrl: string
+  ) => {
+    const cart = { name, id, price, imageUrl };
+    dispatch(cartAddAction(cart));
+    navigate('/cart');
+  };
 
   return (
     <div className="root">
@@ -44,6 +59,8 @@ export default function ProductPage() {
           <Loader />
         ) : (
           <ProductItemDetails
+            id={product?.id || (singleProduct as IProduct)?.id}
+            addToCart={addToCart}
             name={product?.name || (singleProduct as IProduct)?.name}
             price={product?.price || (singleProduct as IProduct)?.price}
             imageUrl={
