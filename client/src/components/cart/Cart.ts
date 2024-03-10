fa7c9e9bce94342572e9876da36ab01c9143a9b0
resetmerlin <resetmerlin@gmail.com>
2024-03-10 16:44:02 +0900

    refactor: Delete Cart header

--- client/src/components/cart/Cart.ts
+++ client/src/components/cart/Cart.ts
@@ -1,11 +1,9 @@
-import CartHeader from './CartHeader';
 import CartLeftGroup from './CartLeftGroup';
 import CartRightGroup from './CartRightGroup';
 import CartSection from './CartSection';
 
 const Cart = {
   Section: CartSection,
-  Header: CartHeader,
   Right: CartRightGroup,
   Left: CartLeftGroup,
 };
