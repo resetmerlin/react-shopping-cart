4217c73ceaa3392dbf90f8d72ea16928e8cc60e0
resetmerlin <resetmerlin@gmail.com>
2024-03-11 10:49:20 +0900

    feat: Add Order List Header molecules component

---
+++ client/src/components/molecules/orderListHeader/OrderListHeader.tsx
@@ -0,0 +1,13 @@
+import styles from './OrderListHeader.module.scss';
+
+type IProps = {
+  id: number;
+};
+export default function OrderListHeader({ id }: IProps) {
+  return (
+    <div className={styles.order__list__header}>
+      <span>주문번호: {id}</span>
+      <span>상세보기 {'>'}</span>
+    </div>
+  );
+}
